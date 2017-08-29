c5030c7e23ed796b2292dc8bac508d4e1b08aae7
Liam Gray <hoxxep@gmail.com>
2017-08-29 23:46:08 +0100

    Add basic doctype comments

--- contracts/TwoFactorAuth.sol
+++ contracts/TwoFactorAuth.sol
@@ -6,17 +6,23 @@ contract TwoFactorAuth {
     string public url;
     string public service;
 
+    /// @dev The event which logs an Ethereum 2FA call that a server can listen to.
     event Authenticated(address _user);
 
+    /// Set the url and service strings on construction.
+    /// @param _url The url this contract is intended to provide 2FA for.
+    /// @param _service The name of the service this contract intends to provide 2FA for.
     function TwoFactorAuth(string _url, string _service) {
         url = _url;
         service = _service;
     }
 
+    /// Default function rejects payments but has enough gas to authenticate users.
     function () external {
         Authenticated(msg.sender);
     }
 
+    /// Authenticate a user.
     function authenticate() {
         Authenticated(msg.sender);
     }
